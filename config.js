42c59b243d054e326e7395b9638ba1c00639aa4c
Antonio Fontaine <afontaine@uc.cl>
2016-09-05 20:22:42 -0300

    -

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/arqui';
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres@52.53.158.9/:5432/arqui';
 
 
 module.exports = connectionString;
