b9c466c60a6f85a34c65cc38599cea8fa6e7a41d
Antonio Fontaine <afontaine@uc.cl>
2016-09-05 20:23:41 -0300

    -

--- database/create.js
+++ database/create.js
@@ -1,9 +1,7 @@
 var pg = require('pg');
 var path = require('path');
 var connectionString = require(path.join(__dirname, '../', 'config'));
-console.log(connectionString);
 var client = new pg.Client(connectionString);
-console.log(client);
 client.connect();
 var query = client.query('CREATE TABLE visits(id SERIAL PRIMARY KEY, os VARCHAR(40), browser VARCHAR(40), ip VARCHAR(40), date TIMESTAMP NOT NULL)');
 query.on('end', function() { client.end(); });
